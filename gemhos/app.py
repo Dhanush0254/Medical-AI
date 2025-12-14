b34439cde4da54c9aa9a57311e55a1ad5b72d52e
Dhanush A <dhanush.a2424@gmail.com>
2025-12-14 17:00:59 +0530

    Update app.py

--- gemhos/app.py
+++ gemhos/app.py
@@ -252,5 +252,5 @@ def predict():
     return jsonify(dict(zip(['predictions', 'suggestions'], predict_risk(request.json))))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+
     app.run(host='0.0.0.0', port=port, debug=True)
